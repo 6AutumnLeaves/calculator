8c613fc925152cbd4b0a164b371ec5e110a6778c
Justus Bartmangit config --global user.email justus.bartman@gmail.com <justus.bartman@gmail.com>
2022-09-21 16:55:08 -0400

    Added prototype math functions

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+const mathFunctions = {
+add: (a, b) => (a + b).toFixed(2),
+subtract: (a, b) => (a - b).toFixed(2),
+multiply: (a, b) => (a * b).toFixed(5),
+divide: (a, b) => (a / b).toFixed(5),
+
+};
+const operate = function(operator, a, b) {
+
+}
