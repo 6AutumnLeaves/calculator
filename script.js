7dd2cf0a594b30e63f5e0a506010050875d200ba
Justus Bartmangit config --global user.email justus.bartman@gmail.com <justus.bartman@gmail.com>
2022-09-22 10:52:38 -0400

    Basic functions finished

--- script.js
+++ script.js
@@ -3,13 +3,10 @@
 
 
 
-const mathFunctions = {
-add: (a, b) => (a + b).toFixed(2),
-subtract: (a, b) => (a - b).toFixed(2),
-multiply: (a, b) => (a * b).toFixed(5),
-divide: (a, b) => (a / b).toFixed(5),
 
-};
-const operate = function(operator, a, b) {
+const add  =(a, b) => (a + b).toFixed(2);
+const subtract = (a, b) => (a - b).toFixed(2);
+const multiply = (a, b) => (a * b).toFixed(5);
+const divide = (a, b) => (a / b).toFixed(5);
 
-}
+const operate = (operator, a, b) => operator(a, b);
